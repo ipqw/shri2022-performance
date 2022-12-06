5eb8ea182106baf7ed90ff9c572a7b91dfc569b0
ipqw <iga538741@gmail.com>
2022-12-06 22:35:59 +0400

    qwe

--- scripts.js
+++ scripts.js
@@ -6,7 +6,9 @@
     }
 
     function makeTabs(node) {
+        let selected = node.querySelector('.section__tab_active').dataset.id;
         const tabs = node.querySelectorAll('.section__tab');
+        const select = node.querySelector('.section__select');
 
         function selectTab(newId) {
             const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
@@ -14,15 +16,46 @@
             const oldTab = node.querySelector('.section__tab_active');
             const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
 
+            selected = newId;
+
             oldTab.classList.remove('section__tab_active');
+            oldTab.setAttribute('aria-selected', 'false');
+            oldTab.removeAttribute('tabindex');
             newTab.classList.add('section__tab_active');
+            newTab.setAttribute('aria-selected', 'true');
+            newTab.setAttribute('tabindex', '0');
+            newTab.focus({
+                preventScroll: true
+            });
+
             oldPanel.classList.add('section__panel_hidden');
+            oldPanel.setAttribute('aria-hidden', 'true');
             newPanel.classList.remove('section__panel_hidden');
+            newPanel.setAttribute('aria-hidden', 'false');
+
+            select.value = newId;
         }
+
+        select.addEventListener('input', () => {
+            selectTab(select.value);
+        });
 
         bind(tabs, 'click', event => {
             const newId = event.target.dataset.id;
             selectTab(newId);
+        });
+    }
+
+    function makeMenu(node) {
+        let expanded = false;
+        const links = document.querySelector('.header__links');
+
+        node.addEventListener('click', () => {
+            expanded = !expanded;
+            node.setAttribute('aria-expanded', expanded ? 'true' : 'false');
+            node.querySelector('.header__menu-text').textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
+            links.classList.toggle('header__links_opened', expanded);
+            links.classList.add('header__links-toggled');
         });
     }
 
